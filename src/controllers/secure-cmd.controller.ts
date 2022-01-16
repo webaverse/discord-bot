e32d84e0f5d52720a736529c3640ba8b98378a84
Hamza Tariq <hamzatrq116@gmail.com>
2022-01-16 06:04:01 +0500

    Commenting unused code

--- src/controllers/secure-cmd.controller.ts
+++ src/controllers/secure-cmd.controller.ts
@@ -5,32 +5,32 @@ import { ethers } from 'ethers';
 import config from '@/config';
 
 async function updateMnemonic(message: Message, user: IUser): Promise<void> {
-  if (message.content.toLowerCase() !== config.botPrefix + 'key reset') {
-    const mnemonic = await userService.generateNewMnemonic(user.id);
-    try {
-      await message.author.send(`Your new mnemonic is: ${mnemonic}`);
-    } catch (error) {
-      if (error.message === 'Cannot send messages to this user') {
-        message.reply('Could not DM you. Please enable DMs from this server.');
-        return;
-      }
-    }
-    return;
-  }
-  const mnemonic = message.content.replace(config.botPrefix + 'key ', '');
-  if (mnemonic.split(' ').length !== 12 || !ethers.utils.isValidMnemonic(mnemonic)) {
-    await message.reply('Invalid mnemonic');
-    return;
-  }
-  await userService.setMnemonic(user.id, mnemonic);
-  try {
-    await message.author.send(`Your new mnemonic is: ${mnemonic}`);
-  } catch (error) {
-    if (error.message === 'Cannot send messages to this user') {
-      message.reply('Could not DM you. Please enable DMs from this server.');
-      return;
-    }
-  }
+  // if (message.content.toLowerCase() !== config.botPrefix + 'key reset') {
+  //   const mnemonic = await userService.generateNewMnemonic(user.id);
+  //   try {
+  //     await message.author.send(`Your new mnemonic is: ${mnemonic}`);
+  //   } catch (error) {
+  //     if (error.message === 'Cannot send messages to this user') {
+  //       message.reply('Could not DM you. Please enable DMs from this server.');
+  //       return;
+  //     }
+  //   }
+  //   return;
+  // }
+  // const mnemonic = message.content.replace(config.botPrefix + 'key ', '');
+  // if (mnemonic.split(' ').length !== 12 || !ethers.utils.isValidMnemonic(mnemonic)) {
+  //   await message.reply('Invalid mnemonic');
+  //   return;
+  // }
+  // await userService.setMnemonic(user.id, mnemonic);
+  // try {
+  //   await message.author.send(`Your new mnemonic is: ${mnemonic}`);
+  // } catch (error) {
+  //   if (error.message === 'Cannot send messages to this user') {
+  //     message.reply('Could not DM you. Please enable DMs from this server.');
+  //     return;
+  //   }
+  // }
 }
 
 export default {
